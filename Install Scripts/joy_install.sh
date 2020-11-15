bf950bd68cb716044852a091c2809dd29a48a3da
RodrigoCatto <you@example.com>
2020-11-15 19:27:36 -0300

    Correction on install files

--- Install Scripts/joy_install.sh
+++ Install Scripts/joy_install.sh
@@ -4,8 +4,4 @@
 sudo apt install ros-melodic-joy
 
 # Intall Twist Joy Pkg to transform controller data to twist msg
-sudo apt install ros-melodic-twist-joy
-
-# Run the launch file and check the commands
-
-#
+sudo apt install ros-melodic-teleop-twist-joy
